a38f3b2a9c4304a70c5b9bb577c510039e41f238
Lev Iva <levivaa@yandex.ru>
2025-12-26 16:12:22 +0100

    Math commit

---
+++ Math.js
@@ -0,0 +1,21 @@
+var num_1 = 5;
+var num_2 = 15;
+
+//var res = num_1 - num_2
+
+console.log("Result: " + (num_1 - num_2));
+
+
+var num_3 = 5;
+//num_3 = num_3 + 7;
+//num_3 += 7;
+num_3++;
+console.log("Result: " + num_3);
+
+var str_1 = Number("12");
+var str_2 = Number("12");
+console.log("Result: " + (str_1 + str_2));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.E);
+console.log("Math: " + Math.min(4,5,6,-7,3,4,1));
