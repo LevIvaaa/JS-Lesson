27070e9c799df34537a78bdfbed341319ee017be
Lev Iva <levivaa@yandex.ru>
2025-12-26 16:53:31 +0100

    Masives commit

---
+++ Masive.js
@@ -0,0 +1,10 @@
+var arr = [5, 2, "stroka", 4, true, 2.5, 0, -100];
+arr[3] = "word";
+console.log(arr.length);
+
+var matrix = [
+    [4, 6, 7], ["stroka", true, 2.5], [2.5, 45, -234]
+];
+
+matrix[1][0] = 90;
+console.log(matrix[1][0]);
