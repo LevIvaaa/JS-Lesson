e56cfec8106e13f724f173a006d2ae84b9dcdf99
Lev Iva <levivaa@yandex.ru>
2025-12-26 15:52:51 +0100

    4 commit

---
+++ variables-datatypes.js
@@ -0,0 +1,16 @@
+//var number;
+//number = 5;
+
+const number = 5;
+//number = 7;
+console.log("Variable: " + number + ".");
+
+var num = "words";
+console.log(num);
+
+var nu = true;
+console.log(nu);
+
+const num_1 = 10;
+const num_2 = 10;
+console.log(num_1 + num_2);
