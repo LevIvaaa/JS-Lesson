f5a474e344ea80a8441332f6ad73f699903e7cad
Lev Iva <levivaa@yandex.ru>
2025-12-27 08:27:34 +0100

    Timers-intervals commit

---
+++ Timers-intervals.js
@@ -0,0 +1,23 @@
+/*var counter = 0
+
+var id =setInterval(my_func, 1000);
+
+function my_func() {
+    counter++
+    console.log(counter);
+
+    if(counter == 3)
+        clearInterval(id);
+}*/
+
+//setInterval(function() {
+//    counter++
+//    console.log(counter)
+
+//}, 1000);
+
+
+setTimeout(function() {
+    console.log("Timer is working");
+    
+}, 2000);
